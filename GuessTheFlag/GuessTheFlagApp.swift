0ae74d9078146ce167f9b2ddc9e8e77cc8b54902
nalin <nalinporwal10@gmail.com>
2024-05-21 18:50:23 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nalin Porwal on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
